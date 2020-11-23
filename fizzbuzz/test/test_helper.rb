636d906df0583789566bf49dfb50bcb4c0afe3c7
David <sanchez.dav90@gmail.com>
2020-11-22 22:24:02 -0600

    Add custom reporter

--- fizzbuzz/test/test_helper.rb
+++ fizzbuzz/test/test_helper.rb
@@ -1,2 +1,6 @@
 require 'minitest/autorun'
+require 'support/reporters'
 require 'minitest/pride'
+# require 'minitest/reporters'
+
+# Minitest::Reporters.use! [Minitest::Reporters::DefaultReporter.new]
