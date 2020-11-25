a90febf2242b47f84b20e7fd85baed29166e1873
David <sanchez.dav90@gmail.com>
2020-11-24 21:31:02 -0600

    Extract Tests to a New Module

--- sharing_code/test/fizz_buzz_test.rb
+++ sharing_code/test/fizz_buzz_test.rb
@@ -2,40 +2,17 @@ require 'test_helper'
 require 'fizz_buzz'
 
 class FizzBuzzTest < Minitest::Test
-  def setup
-    @fb = FizzBuzz.new
-  end
-
-  def test_converts_multiples_of_fifteen_to_fizzbuzz
-    [15, 45, 90].each do |i|
-      assert_equal 'FizzBuzz', @fb.convert(i)
-    end
-  end
-
-  def test_converts_multiples_of_five_to_buzz
-    [5, 20, 100].each do |i|
-      assert_equal 'Buzz', @fb.convert(i)
-    end
-  end
-
-  def test_converts_multiples_of_three_to_fizz
-    [3, 18, 42].each do |i|
-      assert_equal 'Fizz', @fb.convert(i)
-    end
-  end
+  include FizzingBuzzingTests
 
   def test_returns_same_number_for_other_numbers
     [1, 101, 2014].each do |i|
-      assert_equal i.to_s, @fb.convert(i)
+      assert_equal i.to_s, fizz_buzz.convert(i)
     end
   end
 
-  def test_raises_argument_error_for_bad_argument
-    skip 'not yet implemented'
-    assert_raises(ArgumentError) { @fb.convert(-1) }
-    assert_raises(ArgumentError) { @fb.convert(0) }
-    assert_raises(ArgumentError) { @fb.convert(1.0) }
-    assert_raises(ArgumentError) { @fb.convert('foo') }
-    assert_raises(ArgumentError) { @fb.convert(nil) }
+  private
+
+  def fizz_buzz
+    @fizz_buzz ||= FizzBuzz.new
   end
 end
--- sharing_code/test/test_helper.rb
+++ sharing_code/test/test_helper.rb
@@ -3,6 +3,7 @@ require 'support/reporters'
 require 'minitest/pride'
 
 require 'support/fizz_buzz_helpers'
+require 'support/fizzing_buzzing_tests'
 # require 'minitest/reporters'
 
 # Minitest::Reporters.use! [Minitest::Reporters::DefaultReporter.new]
