5420ada963a9bcd55520cbe06ad023eb267da341
David <sanchez.dav90@gmail.com>
2020-11-24 21:15:03 -0600

    Extract Helper Methods into a New Module

--- sharing_code/test/fizz_buzz_iterator_test.rb
+++ sharing_code/test/fizz_buzz_iterator_test.rb
@@ -2,28 +2,21 @@ require 'test_helper'
 require 'fizz_buzz_iterator'
 
 describe FizzBuzzIterator do
+  include FizzBuzzHelpers
+
   subject { FizzBuzzIterator.new }
 
   it 'outputs sequential FizzBuzz values starting from 1' do
-    expected = %w(1 2 Fizz 4 Buzz Fizz)
-    check_iterator_values_over(subject, *expected)
+    expected_values = %w(1 2 Fizz 4 Buzz Fizz)
+    verify_iterator_values(subject, *expected_values)
   end
 
   describe 'when initialized with a starting value' do
     subject { FizzBuzzIterator.new(10) }
 
     it 'outputs sequential FizzBuzz values starting from the starting value' do
-      expected = %w(Buzz 11 Fizz 13 14 FizzBuzz)
-      check_iterator_values_over(subject, *expected)
-    end
-  end
-
-  private
-
-  def check_iterator_values_over(iterator, *values)
-    values.each do |expected_value|
-      next_value = iterator.next
-      expect(next_value).must_equal expected_value
+      expected_values = %w(Buzz 11 Fizz 13 14 FizzBuzz)
+      verify_iterator_values(subject, *expected_values)
     end
   end
 end
--- sharing_code/test/test_helper.rb
+++ sharing_code/test/test_helper.rb
@@ -1,6 +1,8 @@
 require 'minitest/autorun'
 require 'support/reporters'
 require 'minitest/pride'
+
+require 'support/fizz_buzz_helpers'
 # require 'minitest/reporters'
 
 # Minitest::Reporters.use! [Minitest::Reporters::DefaultReporter.new]
